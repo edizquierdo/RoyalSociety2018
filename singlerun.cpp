eaea04da98421f1db1bfe2c7209b45d745a39ab9
mivanit <mivanits@umich.edu>
2021-04-03 23:52:30 -0400

    misc

--- singlerun.cpp
+++ singlerun.cpp
@@ -6,6 +6,8 @@
 
 #define ENABLE_CTOR_JSON 1
 
+#include "packages/cxxopts.hpp"
+
 #include "main.h"
 
 int main (int argc, const char* argv[])
@@ -17,6 +19,26 @@ int main (int argc, const char* argv[])
         angle = stod(argv[1]);
     }
 
+
+    cxxopts::Options options("PhysWormSim", "Mechanical and electrophysiological simulation of C. elegans nematode");
+
+    options.add_options()
+        ("p,params", "params json file", cxxopts::value<std::string>()->default_value("input/params.json"))
+        ("c,coll", "collision tsv file", cxxopts::value<std::string>()->default_value("input/collision_objs.tsv"))
+        ("a,angle", "starting angle", cxxopts::value<double>()->default_value(3.14159 / 2.0))
+        ("s,seed", "random initialization seen", cxxopts::value<int>()->default_value(0))
+        ("o,output", "output dir", cxxopts::value<string>()->default_value("data/run/"))
+        ("h,help", "Print usage")
+    ;
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
+  
     RandomState rs;
     // long seed = static_cast<long>(time(NULL));
     long seed = 0;
