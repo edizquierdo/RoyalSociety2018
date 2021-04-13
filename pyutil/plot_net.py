25601440a239121533b769b2d33b053a6a9d0982
mivanit <mivanits@umich.edu>
2021-04-12 20:09:41 -0400

    misc

--- pyutil/plot_net.py
+++ pyutil/plot_net.py
@@ -1,29 +1,65 @@
-import networkx as nx
+from typing import *
+
+import json
 import matplotlib.pyplot as plt
+import networkx as nx
 
 
-G = nx.DiGraph()
-G.add_node("A")
-G.add_node("B")
-G.add_node("C")
-G.add_node("D")
-G.add_node("E")
-G.add_node("F")
-G.add_node("G")
-G.add_edge("A","B")
-G.add_edge("B","C")
-G.add_edge("C","E")
-G.add_edge("C","F")
-G.add_edge("D","E")
-G.add_edge("F","G")
+def plot_net(
+		params : str = "../input/params/default.json",
+		nervous_systems : Union[str,List[str]] = ["Head", "VentralCord"],
+	):
+	# figure out which nervous systems to plot
+	if isinstance(nervous_systems, str):
+		nervous_systems = nervous_systems.split(',')
 
-print(G.nodes())
-print(G.edges())
+	# load network
+	with open(params, 'r') as fin:
+		data = json.load(fin)
+	
+	# create network
+	G = nx.DiGraph()
+	edges_byType : Dict[str,list] = {
+		'ele' : list(), 'chem' : list(),
+	}
 
-pos = nx.spring_layout(G)
+	for ns in nervous_systems:
+		for nrn in data[ns]["neurons"]:
+			G.add_node(nrn)
 
-nx.draw_networkx_nodes(G, pos)
-nx.draw_networkx_labels(G, pos)
-nx.draw_networkx_edges(G, pos, edge_color='r', arrows = True)
+		if ("n_units" in data[ns]) and (int(data[ns]["n_units"]) > 1):
+			raise NotImplementedError()
+			n_units = int(data[ns]["n_units"])
+			for u in range(n_units):
+				for conn in data[ns]["connections"]:
+					G.add_edge(conn["from"],conn["to"])
+				
+		else:
+			for conn in data[ns]["connections"]:
+				G.add_edge(conn["from"], conn["to"])
+				edges_byType[conn["type"]].append((conn["from"], conn["to"]))
 
-plt.show()
+	print(G.nodes())
+	print(G.edges())
+
+	pos = nx.spring_layout(G)
+
+	nx.draw_networkx_nodes(G, pos, node_size = 1500, node_color = '#E3FFB2')
+	nx.draw_networkx_labels(G, pos)
+	# draw chem (directed)
+	nx.draw_networkx_edges(
+		G, pos, 
+		edgelist = edges_byType['chem'], edge_color='r', 
+		arrows = True, arrowsize = 30, 
+		connectionstyle = 'arc3,rad=0.2',
+		min_target_margin  = 20, 
+	)
+	# draw ele (undirected)
+	nx.draw_networkx_edges(G, pos, edgelist = edges_byType['ele'], edge_color='b', arrows = False)
+
+	plt.show()
+
+
+if __name__ == '__main__':
+	import fire
+	fire.Fire(plot_net)
